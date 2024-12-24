47c4a02252a995908e1926589b7ffc570a40f6bd
user <user@OMAR-PC99>
2024-12-24 21:50:30 +0300

    Add project files.

---
+++ ObjCalculatorHW2/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjCalculatorHW2
+{
+   public class Calculator
+    {
+        public Calculator(int x,int y) {
+            this.x = x; 
+            this.y = y;
+        }
+
+        public int x
+        {
+            get; set;
+        }
+        public int y
+        {
+            get; set;
+        }
+
+        public int Add()
+        {
+            return x + y;
+        }
+        public int subtract()
+        {
+            return x - y;
+        }
+
+        public int multiply()
+        {
+            return x * y;
+        }
+        public int divide()
+        {
+            if ( y == 0)
+            {
+                Console.WriteLine("Cannot Divided By 0");
+            }
+            else
+            {
+  return x / y;
+            }
+            return 0;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ ObjCalculatorHW2/Program.cs
@@ -0,0 +1,20 @@
+﻿using ObjCalculatorHW2;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        int x, y;
+        Console.WriteLine("Enter Num1: ");
+   x=Convert.ToInt32( Console.ReadLine());
+        Console.WriteLine("Enter Num2 : ");
+        y = Convert.ToInt32(Console.ReadLine());
+        Calculator cal1 = new Calculator(x,y);
+
+        Console.WriteLine("Addition : " +cal1.Add());
+        Console.WriteLine("Subtract : " +cal1.subtract());
+        Console.WriteLine("Divide : "+ cal1.divide());
+        Console.WriteLine("Multiply : "+cal1.multiply());
+
+    }
+}
